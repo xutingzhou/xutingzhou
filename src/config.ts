d10aaf61f7afb274eb1989c45c7dd70f3013bd63
xutingzhou <458970@gmail.com>
2023-08-10 07:18:58 +0800

    fix logo size

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,7 @@ export const LOCALE = ["zh-CN"]; // set to [] to use the environment default
 export const LOGO_IMAGE = {
   enable: true,
   svg: true,
-  width: 216,
+  width: 128,
   height: 46,
 };
 
