8f4e42ac20d13e9498c673f588664b2a9b416dea
xutingzhou <458970@gmail.com>
2025-12-08 19:59:45 +0800

    feat: upgrade next 16

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,32 @@
-import rehypePrism from '@mapbox/rehype-prism'
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
+import remarkGfm from "remark-gfm";
+import rehypePrism from "rehype-prism-plus";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  outputFileTracingIncludes: {
-    '/articles/*': ['./src/app/articles/**/*.mdx'],
-    '/life/*': ['./src/app/life/**/*.mdx'],
-  },
+  turbopack: {},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+
   experimental: {
+    mdxRs: false,
     optimizePackageImports: ["@phosphor-icons/react"],
   },
-}
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        {
+          loader: "@mdx-js/loader",
+          options: {
+            remarkPlugins: [[remarkGfm, {}]],
+            rehypePlugins: [[rehypePrism, {}]],
+          },
+        },
+      ],
+    });
+
+    return config;
   },
-})
+};
 
-export default withMDX(nextConfig)
+export default nextConfig;
